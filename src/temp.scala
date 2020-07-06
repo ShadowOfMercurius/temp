269b6d0d474f9dfdeb5fc2bd58817f81925e85c5
zhouhao <530807386@qq.com>
2020-07-06 18:27:24 +0800

    change the function2

--- src/temp.scala
+++ src/temp.scala
@@ -8,6 +8,11 @@ object temp {
     var ans = a*b
     return ans
   }
+
+  def my_js(a:Int,b:Int):Int = {
+    var ans = a-a+520
+    return ans
+  }
   def main(args:Array[String]): Unit ={
 
     var a = 10
@@ -15,8 +20,9 @@ object temp {
     var c = 30
     var res = my_calculate(a,b,c)
     var res2 = my_calculate2(res,a)
+    var res3 = my_js(res2,20)
     println(res2)
-    println("the result = " + res2)
+    println("the result = " + res3)
 
   }
 
