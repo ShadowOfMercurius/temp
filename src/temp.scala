344fa53c1ba870efd40f2cd5954a45b670028628
zhouhao <530807386@qq.com>
2020-07-06 17:56:34 +0800

    change the function

--- src/temp.scala
+++ src/temp.scala
@@ -4,14 +4,19 @@ object temp {
     println(ans)
     return ans
   }
+  def my_calculate2(a:Int,b:Int): Int ={
+    var ans = a*b
+    return ans
+  }
   def main(args:Array[String]): Unit ={
 
     var a = 10
     var b = 20
     var c = 30
     var res = my_calculate(a,b,c)
-    println(res)
-    println("the result = " + res)
+    var res2 = my_calculate2(res,a)
+    println(res2)
+    println("the result = " + res2)
 
   }
 
